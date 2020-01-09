efd596edc0d38bf3bbc3e60e26887c4b123f0bc5
fadizahhar <fadi.zahhar@ogilvy.com>
2020-01-09 16:18:02 +0200

    working with state for the first time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Pokegame from './Pokegame';
+import Game from './Game'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Game />
       <Pokegame />
     </div>
   );
